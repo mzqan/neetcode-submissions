65b65b9e59562cedcd90a41a72292d00da36a62a
Megan Lee <123510256+mzqan@users.noreply.github.com>
2026-07-25 16:03:52 -0400

    Add: jump-game-ii - submission-1

---
+++ Data Structures & Algorithms/jump-game-ii/submission-1.py
@@ -0,0 +1,22 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        steps = 0
+        l = r = 0
+
+        # stop once we've reached end
+        while r < len(nums) - 1:
+            farthest = 0
+
+            # get farthest reachable from THIS idx
+            for i in range(l, r + 1):
+                farthest = max(farthest, i + nums[i])
+
+            # update search to previous step's end/farthest
+            l = r + 1
+
+            # update all-time farthest reachable
+            r = farthest
+
+            # we've taken one step, after advancing form prev farthest
+            steps += 1
+        return steps
