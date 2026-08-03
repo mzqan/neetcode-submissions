763f2f52198d885b14f70fdcd2a5e33a6fb4c573
Megan Lee <123510256+mzqan@users.noreply.github.com>
2026-08-02 22:24:34 -0400

    Add: meeting-schedule - submission-5

---
+++ Data Structures & Algorithms/meeting-schedule/submission-5.py
@@ -0,0 +1,18 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        # sort by start time, ee if it overlaps with prev
+        intervals.sort(key = lambda x : x.start)
+        for i in range(1, len(intervals)):
+            # curr start < prev end -> overlap
+            if intervals[i].start < intervals[i-1].end:
+                return False
+
+        return True
